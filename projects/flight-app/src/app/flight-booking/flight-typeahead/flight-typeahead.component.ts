7090632904d8cdd8b6528bddf0e53bda43dd80c0
mikezks <michael.zikes@intauria.com>
2019-11-05 09:44:21 +0100

    RxJS

--- projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
+++ projects/flight-app/src/app/flight-booking/flight-typeahead/flight-typeahead.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { timer, Observable, Subscription, Subject } from 'rxjs';
-import { tap, share, takeUntil } from 'rxjs/operators';
+import { tap, share, takeUntil, switchMap, debounceTime, filter, distinctUntilChanged, delay } from 'rxjs/operators';
+import { FormControl } from '@angular/forms';
+import { Flight } from '@flight-workspace/flight-api';
+import { HttpParams, HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-flight-typeahead',
@@ -13,10 +16,15 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
   timberSubscription: Subscription;
   destroy$ = new Subject<boolean>();
 
-  constructor() { }
+  control = new FormControl();
+  flights$: Observable<Flight[]>;
+  loading: boolean;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.rxjsDemo();
+    //this.rxjsDemo();
+    this.initTypeahead();
   }
 
   ngOnDestroy(): void {
@@ -37,6 +45,31 @@ export class FlightTypeaheadComponent implements OnInit, OnDestroy {
       .subscribe(console.log);
   }
 
+  initTypeahead(): void {
+    this.flights$ =
+      this.control.valueChanges
+        .pipe(
+          filter(from => from.length > 2),
+          debounceTime(300),
+          distinctUntilChanged(),
+          tap(() => this.loading = true),
+          //delay(1000),
+          switchMap(from => this.load(from)),
+          tap(() => this.loading = false)
+        );
+  }
+
+  load(from: string): Observable<Flight[]>  {
+    let url = "http://www.angular.at/api/flight";
+
+    let params = new HttpParams()
+                        .set('from', from);
+
+    let headers = new HttpHeaders()
+                        .set('Accept', 'application/json');
+
+    return this.http.get<Flight[]>(url, {params, headers});
+  }
 
 
 }
